8e530b2692f75f6f5b8382f75284b8847ef3f98c
MavihRodrigues <mavimariarodrigues@gmail.com>
2025-08-26 15:45:41 -0300

    Crud - sensores

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Livewire\Ambiente\AmbienteCreate;
 use App\Livewire\Dashboard;
+use App\Livewire\SensorDashboard\Sensor as SensorDashboardSensor;
+use App\Models\Sensor;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+
+Route::get('/sensor', SensorDashboardSensor::class);
